b70faf207cf1c82f7bd3c49c69799c58ad26ea6a
Nick <nicholas.j.campbell@gmail.com>
2009-06-22 23:21:21 -0400

    Initial commit

---
+++ vlc_cam.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+vlc v4l2:// :v4l-vdev="/dev/video0" :v4l-adev="/dev/audio2" :v4l-norm=3 :v4l-frequency=-1 :v4l-caching=300 :v4l-chroma="" :v4l-fps=-1.000000 :v4l-samplerate=44100 :v4l-channel=0 :v4l-tuner=-1 :v4l-audio=-1 :v4l-stereo :v4l-width=640 :v4l-height=480 :v4l-brightness=-1 :v4l-colour=-1 :v4l-hue=-1 :v4l-contrast=-1 :no-v4l-mjpeg :v4l-decimation=1 :v4l-quality=100
