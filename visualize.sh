9d2a17d54d4707ed11a5f4625101f21e3f8ddf4d
Nick Campbell <nicholas.j.campbell@gmail.com>
2010-04-06 22:02:28 -0400

    Attempts at modularity.
    Signed-off-by: Nick Campbell <nicholas.j.campbell@gmail.com>

--- visualize.sh
+++ visualize.sh
@@ -4,9 +4,16 @@ DIR=$1
 FORMAT=${2:-'pdf'}
 FILENAME="hierarchy.$FORMAT"
 
+TYPE="--java"
+if [ -n "$3" ]; then
+    if [ "$3" == "scala" ]; then
+	TYPE="-a"
+    fi
+fi
+
 cat \
   <(echo 'digraph types {') \
-  <(ack-grep '(class|interface) \w+ (extends|implements) \w+' --java -h -o $DIR |  \
+  <(ack-grep '(class|interface) \w+ (extends|implements) \w+' "$TYPE" -h -o $DIR |  \
     awk '{print "\"" $4 "\"", "->", "\"" $2 "\""}') \
-  <(echo '}') \
-  | dot -T$FORMAT >$FILENAME && okular $FILENAME
+  <(echo '}') #\
+#  | dot -T$FORMAT >$FILENAME && okular $FILENAME
